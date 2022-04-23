881ab58c15b1ddddb0426d4dd9885058ffcbfd2f
Abdelwahab <ab28fb@gmail.com>
2022-04-23 01:37:39 +0100

    WIP barcode scanner

--- src/app/services/bar-code.service.ts
+++ src/app/services/bar-code.service.ts
@@ -16,7 +16,6 @@ export class BarCodeService {
 
   getBarCodeFromGreyLevelMatrix(matrix: number[][]): string {
     this.utils.convertMatrixToBinary(matrix);
-    // console.log(matrix[30]);
     let pattern: number[] = this.getPattern(matrix[30]);
     return this.getBarCodeFromPattern(pattern);
   }
@@ -24,194 +23,193 @@ export class BarCodeService {
   getBarCodeFromPattern(pattern: number[]): string {
     console.log(pattern);
     let barCode: string = "";
-    let encoding = "";
-    let iteration = 1;
-    let encodingDigit = 0;
-    for (let i = 0; i < pattern.length; i++) {
-      encoding +=""+ pattern[i];
-      encodingDigit++;
-      if (iteration == 1 || iteration == 7) {
-        if (encodingDigit == 3) {
-          encoding = "";
-          encodingDigit = 0;
-          iteration++;
-        }
-      } else {
-        if (encodingDigit == 7) {
-          barCode += this.getDigitFromBinaryEncoding(encoding);
-          encoding = "";
-          encodingDigit = 0;
-          iteration++;
-        }
+    if (this.checkPattern(pattern)) return "can not read barcode from this image";
+    else {
+      pattern=this.cropGuards(pattern);
+      console.log(pattern);
+      for (let i = 0; i < 12; i++) {
+        barCode+=this.getDigitFromBinaryEncoding(pattern.slice(i*7,(i*7)+7).join(''));
       }
-
+      return barCode;
     }
-    return barCode;
+  }
+  private cropGuards(pattern:number[]):number[]{
+    let croppedPattern:number[];
+    croppedPattern=pattern.slice(3,92);
+    croppedPattern=croppedPattern.slice(0,Math.floor(croppedPattern.length/2)-2).concat(croppedPattern.slice(Math.floor(croppedPattern.length/2)+3));
+    return croppedPattern;
+  }
+  private checkPattern(pattern: number[]): boolean {
+    let firstGuard = (pattern[0] == 0 && pattern[1] == 1 && pattern[2] == 0);
+    let lastGuard = (pattern[pattern.length - 1] == 0 && pattern[pattern.length - 2] == 1 && pattern[pattern.length - 3] == 0);
+    let middleGuard = (pattern[44] == 0 && pattern[45] == 1 && pattern[46] == 0 && pattern[47] == 1 && pattern[48] == 0);
+    console.log("guard check : "+firstGuard && middleGuard && lastGuard);
+    return firstGuard && middleGuard && lastGuard;
+
   }
 
   getDigitFromBinaryEncoding(encoding: String): string {
     console.log(encoding);
     let digit: string;
-      switch (encoding) {
-        //-------------L-CODE
-        case "0001101":
-          digit = "0";
-          break;
-        case "0011001":
-          digit = "1";
-          break;
-        case "0010011":
-          digit = "2";
-          break;
-        case "0111101":
-          digit = "3";
-          break;
-        case "0100011":
-          digit = "4";
-          break;
-        case "0110001":
-          digit = "5";
-          break;
-        case "0101111":
-          digit = "6";
-          break;
-        case "0111011":
-          digit = "7";
-          break;
-        case "0110111":
-          digit = "8";
-          break;
-        case "0001011":
-          digit = "9";
-          break;
-          //-------------------G-CODE
-        case "0100111":
-          digit = "0";
-          break;
-        case "0110011":
-          digit = "1";
-          break;
-        case "0011011":
-          digit = "2";
-          break;
-        case "0100001":
-          digit = "3";
-          break;
-        case "0011101":
-          digit = "4";
-          break;
-        case "0111001":
-          digit = "5";
-          break;
-        case "0000101":
-          digit = "6";
-          break;
-        case "0010001":
-          digit = "7";
-          break;
-        case "0001001":
-          digit = "8";
-          break;
-        case "0010111":
-          digit = "9";
-          break;
-          //-------------R-CODE
-        case "1110010":
-          digit = "0";
-          break;
-        case "1100110":
-          digit = "1";
-          break;
-        case "1101100":
-          digit = "2";
-          break;
-        case "1000010":
-          digit = "3";
-          break;
-        case "1011100":
-          digit = "4";
-          break;
-        case "1001110":
-          digit = "5";
-          break;
-        case "1010000":
-          digit = "6";
-          break;
-        case "1000100":
-          digit = "7";
-          break;
-        case "1001000":
-          digit = "8";
-          break;
-        case "1110100":
-          digit = "9";
-          break;
-        default:digit="X";break;
+    switch (encoding) {
+      //-------------L-CODE
+      case "0001101":
+        digit = "0";
+        break;
+      case "0011001":
+        digit = "1";
+        break;
+      case "0010011":
+        digit = "2";
+        break;
+      case "0111101":
+        digit = "3";
+        break;
+      case "0100011":
+        digit = "4";
+        break;
+      case "0110001":
+        digit = "5";
+        break;
+      case "0101111":
+        digit = "6";
+        break;
+      case "0111011":
+        digit = "7";
+        break;
+      case "0110111":
+        digit = "8";
+        break;
+      case "0001011":
+        digit = "9";
+        break;
+      //-------------------G-CODE
+      case "0100111":
+        digit = "0";
+        break;
+      case "0110011":
+        digit = "1";
+        break;
+      case "0011011":
+        digit = "2";
+        break;
+      case "0100001":
+        digit = "3";
+        break;
+      case "0011101":
+        digit = "4";
+        break;
+      case "0111001":
+        digit = "5";
+        break;
+      case "0000101":
+        digit = "6";
+        break;
+      case "0010001":
+        digit = "7";
+        break;
+      case "0001001":
+        digit = "8";
+        break;
+      case "0010111":
+        digit = "9";
+        break;
+      //-------------R-CODE
+      case "1110010":
+        digit = "0";
+        break;
+      case "1100110":
+        digit = "1";
+        break;
+      case "1101100":
+        digit = "2";
+        break;
+      case "1000010":
+        digit = "3";
+        break;
+      case "1011100":
+        digit = "4";
+        break;
+      case "1001110":
+        digit = "5";
+        break;
+      case "1010000":
+        digit = "6";
+        break;
+      case "1000100":
+        digit = "7";
+        break;
+      case "1001000":
+        digit = "8";
+        break;
+      case "1110100":
+        digit = "9";
+        break;
+      default:
+        digit = "X";
+        break;
     }
     return digit;
   }
 
   private getPattern(imageRow: number[]): number[] {
     let pattern: number[] = [];
-    let areasLengths: number[] = [];
-    let areaWidth: number = this.getAreaWidth(imageRow);
-    let started = false;
-    let currentArea: number = 1;
-    let currentAreaLength: number = 0;
-    for (let i = 0; i < imageRow.length; i++) {
-      if (imageRow[i] == 1 && !started) continue;
-      started = true;
-      if (currentArea != imageRow[i]) {
-        if (currentAreaLength != 0) areasLengths.push(currentAreaLength);
-        currentAreaLength = 0;
-        currentArea = imageRow[i];
-      } else {
-        currentAreaLength++;
-      }
-    }
-    let rounds = 0;
-    for (let i = 0; i < areasLengths.length; i++) {
-      rounds = Math.floor(areasLengths[i] / (areaWidth - (areaWidth * 0.2)));
-      for (let j = 0; j < rounds; j++) {
-        pattern.push((i +1) % 2);
-      }
+    imageRow = this.normaliseImageRow(imageRow);
+    // console.log(imageRow);
+    let areaWidth: number = imageRow.length / 95;
+    for (let i = 0; i < 95; i++) {
+      pattern[i] = this.getAreaValue(imageRow.slice(i * areaWidth, (i * areaWidth) + areaWidth));
     }
     return pattern;
   }
 
-  private getAreaWidth(imageRow: number[]): number {
-    let areaWidth = 0;
-    // let finalAreaWidth = 0;
-    // let currentValue = 1;
-    // let init = 0;
-    // let isCalculating = false;
-    // for (let i = 0; (i < imageRow.length && init < 4); i++) {
-    //   if (currentValue != imageRow[i]) {
-    //     finalAreaWidth = Math.max(finalAreaWidth, areaWidth);
-    //     areaWidth = 0;
-    //     isCalculating = true;
-    //     areaWidth++;
-    //     currentValue = imageRow[i];
-    //     init++;
-    //     continue;
-    //   }
-    //   if (isCalculating) {
-    //     areaWidth++;
-    //   }
-    // }
-    // return finalAreaWidth;
-    let startingIndex=0;
-    for (let i = 0; i < imageRow.length; i++) {
-      if(imageRow[i]===0){
-        startingIndex=i;
-        break;
-      }
+  private getAreaValue(area: number[]): number {
+    let blackValue = 0, whiteValue = 0;
+    for (let i = 0; i < area.length; i++) {
+      if (area[i] == 0) blackValue++;
+      else if (area[i] == 1) whiteValue++;
+      else console.log("something is wrong with this pixel its value is : " + area[i]);
     }
-    for (let i = startingIndex; i < imageRow.length; i++) {
-      if(imageRow[i]===1)break;
-      areaWidth++;
+
+    if (blackValue > whiteValue) return 1;
+    else if (whiteValue > blackValue) return 0;
+    else {
+      console.log("something is wrong white and black pixels are equal in this area :" + area);
+      return -1;
     }
-    console.log(areaWidth);
-    return areaWidth;
   }
+
+
+  private normaliseImageRow(imageRow: number[]): number[] {
+    let firstIndex = 0;
+    let lastIndex = imageRow.length - 1;
+    while (true) {
+      if (imageRow[firstIndex] == 0) break;
+      firstIndex++;
+    }
+    while (true) {
+      if (imageRow[lastIndex] == 0) break;
+      lastIndex--;
+    }
+    // console.log(firstIndex);
+    // console.log(lastIndex);
+    return imageRow.slice(firstIndex, lastIndex + 1);
+  }
+  //
+  // private getAreaWidth(imageRow: number[]): number {
+  //   let areaWidth = 0;
+  //   let startingIndex = 0;
+  //   for (let i = 0; i < imageRow.length; i++) {
+  //     if (imageRow[i] === 0) {
+  //       startingIndex = i;
+  //       break;
+  //     }
+  //   }
+  //   for (let i = startingIndex; i < imageRow.length; i++) {
+  //     if (imageRow[i] === 1) break;
+  //     areaWidth++;
+  //   }
+  //   console.log(areaWidth);
+  //   return areaWidth;
+  // }
 }
